b67e2f725afe50aa4d77882043adcef5dc229b95
RaulBecerraB <ingenierobecerrabarcelo@gmail.com>
2025-01-05 06:46:14 -0600

    Complete excercise 2.6

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,34 @@ const App = () => {
   ])
   const [newName, setNewName] = useState('')
 
+  function addPerson (event) {
+    event.preventDefault()
+    const personObject = {
+      name: newName
+    }
+
+    setPersons(persons.concat(personObject))
+  }
+
+  function handlePersonChange (event) {
+    setNewName(event.target.value)
+  }
+
   return (
     <div className='m-6 p-4 bg-white rounded-lg shadow-md shadow-black'>
       <h1 className='text-2xl font-semibold mb-4'>Phonebook</h1>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handlePersonChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul>
+        {persons.map((person, index) => <li key={index}>{person.name}</li>)}
+      </ul>
     </div>
   )
 }
