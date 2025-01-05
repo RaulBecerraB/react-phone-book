f60e8bef52e4d4d7ed2029c756aa897c21a880ea
RaulBecerraB <ingenierobecerrabarcelo@gmail.com>
2025-01-05 06:30:58 -0600

    Add program base version

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [newName, setNewName] = useState('')
+
+  return (
+    <div className='m-6 p-4 bg-white rounded-lg shadow-md shadow-black'>
+      <h1 className='text-2xl font-semibold mb-4'>Phonebook</h1>
+      <form>
+        <div>
+          name: <input />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      ...
+    </div>
+  )
+}
+
+export default App
